b1c1d5b5551047bff8815e63aea742305e879cef
abr <1103137123@gm.kuas.edu.tw>
2017-03-03 14:09:39 +0800

    fix bug

--- abr/Program.cs
+++ abr/Program.cs
@@ -17,6 +17,8 @@ namespace abr
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+
+            //fix
         }
     }
 }
